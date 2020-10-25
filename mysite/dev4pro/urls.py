7b80f8443639395610d9ff95e197d14aa5acbd87
yzjazz <yzjazz@163.com>
2020-10-25 17:22:23 +0800

    my first django project - manager

---
+++ mysite/dev4pro/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+
+from . import wow
+
+# 把
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+
+    path('hello/', wow.hello, name='hello'),
+]
